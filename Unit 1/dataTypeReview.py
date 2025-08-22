00dcb85959af30072e293ad79dcf98e3d601b68a
LiamNelson26 <lnelson490@boyslatin.net>
2025-08-22 15:09:10 +0000

    Classwork 8/22/25

---
+++ Unit 1/dataTypeReview.py
@@ -0,0 +1,36 @@
+print('Liam Nelson')
+
+# W3Schools.com - our friend
+
+# 1.
+# print each of the following data types:
+# string
+print("Hello, World")
+
+console.log("Hello World")
+
+# boolean
+print(2 > 1)
+print(True)
+b = 3
+a = 2
+print(b > a)
+
+print(True)
+print(False)
+
+# integer
+print(1)
+print(1234)
+z = 39394873
+print(z)
+
+# float
+print(10.00)
+print(3.14)
+
+# 2.
+# Create and print a list that features 2 of eaach data type.
+food = ["potato","salad","candy"]
+
+print([True, False, 10, 100, 10.5, 10.3, "I like apples", "4"])
